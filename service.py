7ff9aeef245a644dbd5f0992ef9e873a5c815546
VVNoodle <brickkace@gmail.com>
2019-05-03 16:16:48 -0700

    edit homepage, README, error status code

--- service.py
+++ service.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify, json, render_template
 from fetch_data import extract_data
 import pandas as pd
 import sys
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "HI"
+    return render_template("index.html")
 
 
 @app.route('/commodity')
@@ -16,24 +16,23 @@ def commodity():
     start_date = request.args["start_date"] if "start_date" in request.args else None
     end_date = request.args["end_date"] if "end_date" in request.args else None
     commodity_type = request.args["commodity_type"] if "commodity_type" in request.args else None
-    if commodity_type == None:
-        return "You must provide a commodity type"
+    if commodity_type == None or start_date == None or end_date == None:
+        return "You must provide commodity_type, start_date and end_date params", 422
     commodity = pd.DataFrame()
     if commodity_type == "gold":
         commodity = extract_data("gold")
     elif commodity_type == "silver":
         commodity = extract_data("silver")
 
-    commodity = commodity[pd.to_datetime(commodity['Date']).dt.date.astype(
-        str) >= start_date] if start_date != None else commodity
+    converted_date = pd.to_datetime(commodity['Date']).dt.date.astype(str)
+    mask = (converted_date >= start_date) & (converted_date <= end_date)
+    commodity = commodity.loc[mask]
 
-    commodity = commodity[pd.to_datetime(commodity['Date']).dt.date.astype(
-        str) <= end_date] if start_date != None else commodity
-
+    # set index column as Date, for formatting requirements of the exercise
     commodity = commodity.set_index("Date")
-    time = json.loads(commodity.to_json())["Price"]
+    data = json.loads(commodity.to_json())["Price"]
     return jsonify(
-        time=time,
+        data=data,
         mean=commodity.mean()["Price"],
         variance=commodity.var()["Price"])
 
